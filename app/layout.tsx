405e09cc5285b8d0b9b10aaf1a5dec95b395964c
vineet <vvineet.kumar21@gmail.com>
2024-06-15 17:23:55 +0530

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import 'bootstrap-icons/font/bootstrap-icons.css'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,41 @@
 'use client';
-
 import { useChat } from 'ai/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Markdown from './component/markdown';
 export default function Home() {
   const { messages, input, handleInputChange, handleSubmit, isLoading, stop } = useChat({
-    api:'api/geminiai'
+    api: 'api/geminiai'
   });
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12">
- {RenderForm()}
- {RenderMessages()}
- {JSON.stringify(messages)}  {/* since message are in json format so we convert it into string */}
-
+      {RenderForm()}
+      {RenderMessages()}
+      
     </main>
   );
-  function RenderForm(){
-  
+  function RenderForm() {
+
     return <form onSubmit={(event) => {
       event.preventDefault();
       handleSubmit(event, {
-        data:{
+        data: {
           prompt: input
         }
       })
     }} className="w-full flex flex-row gap-2 items-center h-full" >
-      <input type="text" value={input} onChange={handleInputChange} placeholder={isLoading ?"generating" : "ask something......"} disabled={isLoading} className="border-b border-dashed outline-none w-full px-4 py-2 text-[#8428A0] placeholder:text-[#8428A099] text-right focus:placeholder-transparent"/>
-      <button type="submit" className="rounded-full shadow-md p-2 flex flex-row ">send</button>
+      <input type="text" value={input} onChange={handleInputChange} placeholder={isLoading ? "generating" : "ask something......"} disabled={isLoading} className="border-b border-dashed outline-none w-full px-4 py-2 text-[#8428A0] placeholder:text-[#8428A099] text-right focus:placeholder-transparent" />
+      <button type="submit"  className="rounded-full shadow-md px-2 p-1 flex flex-row border" style={{border:"solid",color:'#8428A099'}}>{isLoading ?<i className="bi bi-arrow-clockwise animate-spin"></i> :<i className="bi bi-send-fill"></i>} </button>
     </form>
   }
-  
+
   function RenderMessages() {
     return <div className='flex flex-col-reverse w-full text-left mt-4 gap-4 whitespace-pre-wrap'>
       {
-        messages.map((mes, index) =>{
-          return <><div className={`p-4 shadow-md rounded-md ml-10 relative ${mes.role === 'user'? "bg-stone-300":""}`}>
-            {mes.content}
-            {mes.role === 'user' ? (<h5 className='absolute top-2 -left-19'>USER</h5>) : (<h5 className='absolute top-2 -left-19'>Gemini</h5>)}
+        messages.map((mes, index) => {
+          return <><div id='chatbox' className={`p-4 shadow-md rounded-md ml-10 relative ${mes.role === 'user' ? "bg-stone-300" : ""}`}>
+            <Markdown text= {mes.content} />
+            {mes.role === 'user' ? (<i className="bi bi-person-arms-up absolute top-2 -left-10" style={{color: 'red'}}></i>) : (<i className={`bi  bi-robot absolute -left-10 top-2 ${isLoading && index === messages.length-1 ? 'animate-bounce':''}`}  style={{color:'blue'}}/>
+          )}
           </div></>
         })
       }
