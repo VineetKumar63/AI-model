cea1671f780305d827221edb754434640af02435
vineet <vvineet.kumar21@gmail.com>
2024-06-19 17:57:00 +0530

    adding database

--- app/api/geminiai/route.ts
+++ app/api/geminiai/route.ts
@@ -1,11 +1,41 @@
-import { GoogleGenerativeAIStream, StreamingTextResponse, streamToResponse } from "ai";
+
+import { GoogleGenerativeAIStream, StreamingTextResponse } from "ai";
 import { google } from '@ai-sdk/google';
 import {GoogleGenerativeAI} from "@google/generative-ai"
+import { AIStream } from "ai"
+import supabase from "@/utils/supabaseClient/server";
+import { useChat } from "ai/react";
+
+async function promptToDatabase(prompt: string) {
+    const { error } = await supabase.from("chat_messages").insert({user_type:"person", messages: prompt });
+    if (error) {
+      console.error('Error storing prompt:', error);
+    }
+  }
+
+async function saveCompletionToDatabase(completion: string) {
+    const { error } = await supabase.from('chat_messages').insert({ messages: completion });
+    if (error) {
+      console.error('Error storing completion:', error);
+    }
+    }
+
 export async function POST(req:Request, res: Response) {
         const reqBody = await req.json();
     const propmt = reqBody.data.prompt;
  const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY!)
- const model = genAI.getGenerativeModel({model:'gemini-1.5-flash'})
+ const model = genAI.getGenerativeModel({model:'gemini-1.5-pro-latest'})
  const streamingResponse = await model.generateContentStream(propmt)
- return new StreamingTextResponse(GoogleGenerativeAIStream(streamingResponse))
+ return new StreamingTextResponse(GoogleGenerativeAIStream(streamingResponse,{
+    onStart: async() =>{
+        await promptToDatabase(propmt)
+    },
+    onToken: async(token:string)=>{
+        console.log(token)
+    },
+    onCompletion: async(competion:string)=>{
+        await saveCompletionToDatabase(competion)
+    }
+ }))
 }
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,78 @@
+
 'use client';
 import { useChat } from 'ai/react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Markdown from './component/markdown';
+import { createServerClient } from '@supabase/ssr';
+import supabase from '@/utils/supabaseClient/server';
+
 export default function Home() {
-  const { messages, input, handleInputChange, handleSubmit, isLoading, stop } = useChat({
+  const { messages, input, handleInputChange, handleSubmit, isLoading, stop, } = useChat({
     api: 'api/geminiai'
   });
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-12">
+    <main >
+      <div>
+      </div>
+      <div className="flex min-h-screen flex-col items-center justify-between p-12">
       {RenderForm()}
       {RenderMessages()}
-      
+      </div>
     </main>
   );
-  function RenderForm() {
 
-    return <form onSubmit={(event) => {
-      event.preventDefault();
-      handleSubmit(event, {
-        data: {
-          prompt: input
-        }
-      })
-    }} className="w-full flex flex-row gap-2 items-center h-full" >
-      <input type="text" value={input} onChange={handleInputChange} placeholder={isLoading ? "generating" : "ask something......"} disabled={isLoading} className="border-b border-dashed outline-none w-full px-4 py-2 text-[#8428A0] placeholder:text-[#8428A099] text-right focus:placeholder-transparent" />
-      <button type="submit"  className="rounded-full shadow-md px-2 p-1 flex flex-row border" style={{border:"solid",color:'#8428A099'}}>{isLoading ?<i className="bi bi-arrow-clockwise animate-spin"></i> :<i className="bi bi-send-fill"></i>} </button>
-    </form>
+  function RenderForm() {
+    return (
+      <form
+        onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
+          event.preventDefault();
+           
+          handleSubmit(event, {
+            data: {
+              prompt: input
+            }
+          });
+        }} className="w-full flex flex-row gap-2 items-center h-full" >
+        <input
+          type="text"
+          value={input}
+          onChange={handleInputChange}
+          placeholder={isLoading ? "generating" : "ask something......"}
+          disabled={isLoading}
+          className="border-b border-dashed outline-none w-full px-4 py-2 text-[#8428A0] placeholder:text-[#8428A099] text-right focus:placeholder-transparent"
+        />
+        <button
+          type="submit"
+          className="rounded-full shadow-md px-2 p-1 flex flex-row border"
+          style={{ border: "solid", color: '#8428A099' }}>
+          {isLoading ? (
+            <i className="bi bi-arrow-clockwise animate-spin"></i>
+          ) : (
+            <i className="bi bi-send-fill"></i>)}
+        </button>
+      </form>)
   }
 
   function RenderMessages() {
-    return <div className='flex flex-col-reverse w-full text-left mt-4 gap-4 whitespace-pre-wrap'>
-      {
-        messages.map((mes, index) => {
-          return <><div id='chatbox' className={`p-4 shadow-md rounded-md ml-10 relative ${mes.role === 'user' ? "bg-stone-300" : ""}`}>
-            <Markdown text= {mes.content} />
-            {mes.role === 'user' ? (<i className="bi bi-person-arms-up absolute top-2 -left-10" style={{color: 'red'}}></i>) : (<i className={`bi  bi-robot absolute -left-10 top-2 ${isLoading && index === messages.length-1 ? 'animate-bounce':''}`}  style={{color:'blue'}}/>
-          )}
-          </div></>
-        })
-      }
-    </div>
+    return (
+      <div className='flex flex-col-reverse w-full text-left mt-4 gap-4 whitespace-pre-wrap'>
+        {messages.map((mes, index) => (
+          <div
+            key={index}
+            id='chatbox'
+            className={`p-4 shadow-md rounded-md ml-10 relative ${mes.role === 'user' ? "bg-stone-300" : ""}`}
+          >
+            <Markdown text={mes.content} />
+            {mes.role === 'user' ? (
+              <i className="bi bi-person-arms-up absolute top-2 -left-10" style={{ color: 'red' }}></i>
+            ) : (
+              <i className={`bi  bi-robot absolute -left-10 top-2 ${isLoading && index === messages.length - 1 ? 'animate-bounce' : ''}`} style={{ color: 'blue' }}
+              />
+            )}
+          </div>
+        ))
+        }
+      </div>)
   }
 }
-
